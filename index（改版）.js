b1f3326d5085fc8a2399a72d43409df82754e848
yangleiily <148935776+yangleiily520@users.noreply.github.com>
2025-01-11 22:54:09 +0800

    Add files via upload

---
+++ index（改版）.js
@@ -0,0 +1,186 @@
+/**
+ *  自定义网站配置 
+ */
+const config = {
+  title: "冰宝路导航站",                 //write your website title
+  subtitle: "Bingbao Road Navigation Station", //write your website subtitle
+  logo_icon: "sitemap",               //select your logo by semantic-ui icon (you can get more msg in:https://semantic-ui.com/elements/icon.html)
+  search:true,                        //enable search function
+  search_engine:[                     //choose search engine which you use
+    {
+      name:"百 度",
+      template:"https://www.baidu.com/s?wd=$s"
+    },
+    {
+      name:"谷 歌",
+      template:"https://www.google.com/search?q=$s"
+    },
+    {
+      name:"必 应",
+      template:"https://www.bing.com/search?q=$s"
+    },
+    {
+      name:"搜 狗",
+      template:"https://www.sogou.com/web?query=$s"
+    }
+  ],
+  lists: [                            //Url list
+    {
+      name:"技术",
+      icon:"code",
+      list:[
+        {
+          url:"https://www.cloudflare-cn.com",
+          name:"cloudflare",
+          desc:"科赋锐信息科技"
+        },
+        {
+          url:"https://youtube.com",
+          name:"YouTube",
+          desc:"油管视频"
+        },
+        {
+          url:"https://github.com",
+          name:"GitHub",
+          desc:"GitHub项目部署"
+        },
+        {
+          url:"https://gitee.com",
+          name:"Gitee",
+          desc:"国内代码部署"
+        },
+      ]
+    },
+    {
+      name:"学习",
+      icon:"graduation cap",
+      list:[
+        {
+          url:"https://cn.wordpress.org/download/releases",
+          name:"Wordpress中文包",
+          desc:""
+        },
+        {
+          url:"https://www.bt.cn/",
+          name:"国内流行宝塔运维面板",
+          desc:""
+        },
+        {
+          url:"https://www.aapanel.com/new/index.html/",
+          name:"宝塔面板国际版",
+          desc:""
+        },
+        {
+          url:"https://cli.im/",
+          name:"草料二维码生成器",
+          desc:""
+        },
+      ]
+    }
+  ]
+}
+const el = (tag, attrs, content) => `<${tag} ${attrs.join(" ")}>${content}</${tag}>`;
+
+async function handleRequest(request) {
+  const init = {
+    headers: {
+      'content-type': 'text/html;charset=UTF-8',
+    },
+  }
+  return new Response(renderHTML(renderIndex(),config.selling_ads? renderSeller() :null), init);
+}
+addEventListener('fetch', event => {
+  return event.respondWith(handleRequest(event.request))
+})
+
+/*通过分析链接 实时获取favicon
+* @url 需要分析的Url地址
+*/
+function getFavicon(url) {
+  const domain = url.replace(/^https?:\/\//, '').split('/')[0];
+  return `https://icons.duckduckgo.com/ip3/${domain}.ico`;
+}
+
+/** Render Functions
+ *  渲染模块函数
+ */
+
+function renderIndex(){
+  const footer = el('footer',[],el('div',['class="footer"'],'冰宝路导航站' + el('a',['class="ui label"','href="https://github.com/yangleiily520/CF-Worker-Dir"','target="_blank"'],el('i',['class="github icon"'],"") + 'Cf-Worker-Dir') + ' &copy; Base on ' + el('a',['class="ui label"'],el('i',['class="balance scale icon"'],"") + 'yangleiily@gmail.com')));
+  return renderHeader() + renderMain() + footer;
+}
+
+function renderHeader(){
+  const item = (template,name) => el('a',['class="item"',`data-url="${template}"`],name);
+
+  var nav = el('div',['class="ui large secondary inverted menu"'],el('div',['class="item"'],el('p',['id="hitokoto"'],'条条大路通罗马')))
+  var title = el('h1',['class="ui inverted header"'],el('i',[`class="${config.logo_icon} icon"`],"") + el('div',['class="content"'],config.title + el('div',['class="sub header"'],config.subtitle)));
+  var menu = el('div',['id="sengine"','class="ui bottom attached tabular inverted secondary menu"'],el('div',['class="header item"'],'&nbsp;') + config.search_engine.map((link,key) =>{
+    if(key == 0){
+      return el('a',['class="active item"',`data-url="${link.template}"`],link.name);
+    }else{
+      return item(link.template,link.name);
+    }
+  }).join(""))
+  var input = el('div',['class="ui left corner labeled right icon fluid large input"'],el('div',['class="ui left corner label"'],el('img',['id="search-fav"','class="left floated avatar ui image"','src="https://www.baidu.com/favicon.ico"'],"")) + el('input',['id="searchinput"','type="search"','placeholder="搜索你想要知道的……"','autocomplete="off"'],"") + el('i',['class="inverted circular search link icon"'],""));
+  return el('header',[],el('div',['id="head"','class="ui inverted vertical masthead center aligned segment"'],(config.hitokoto ? el('div',['id="nav"','class="ui container"'],nav) : "") + el('div',['id="title"','class="ui text container"'],title + (config.search ? input + menu :"") + `${config.selling_ads ? '<div><a id="menubtn" class="red ui icon inverted button"><i class="heart icon"></i> 喜欢此域名 </a></div>' : ''}`)))
+}
+
+function renderMain() {
+  var main = config.lists.map((item) => {
+    const card = (url,name,desc)=> el('a',['class="card"',`href=${url}`,'target="_blank"'],el('div',['class="content"'],el('img',['class="left floated avatar ui image"',`src=${getFavicon(url)}`],"") + el('div',['class="header"'],name) + el('div',['class="meta"'],desc)));
+    const divider = el('h4',['class="ui horizontal divider header"'],el('i',[`class="${item.icon} icon"`],"")+item.name);
+
+    var content = el('div',['class="ui four stackable cards"'],item.list.map((link) =>{
+      return card(link.url,link.name,link.desc);
+    }).join(""));
+
+    return el('div',['class="ui basic segment"'],divider + content);
+  }).join("");
+  
+  return el('main',[],el('div',['class="ui container"'],main));
+}
+
+function renderHTML(index,seller) {
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>${config.title} - ${config.subtitle}</title>
+      <link href="https://cdn.jsdelivr.net/npm/semantic-ui-css@2.4.1/semantic.min.css" rel="stylesheet">
+      <link href="https://cdn.jsdelivr.net/gh/sleepwood/cf-worker-dir@0.1.1/style.css" rel="stylesheet">
+      <script src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/semantic-ui-css@2.4.1/semantic.min.js"></script>
+  </head>
+  <body>
+    ${index}
+    ${config.selling_ads ? seller : ''}
+    <script src="https://v1.hitokoto.cn/?encode=js&select=%23hitokoto" defer></script>
+    <script>
+      $('#sengine a').on('click', function (e) {
+        $('#sengine a.active').toggleClass('active');
+        $(e.target).toggleClass('active');
+        $('#search-fav').attr('src',$(e.target).data('url').match(`+/https{0,1}:\/\/\S+\//+`)[0] + '/favicon.ico') ;
+      });
+      $('.search').on('click', function (e) {
+          var url = $('#sengine a.active').data('url');
+          url = url.replace(`+/\$s/+`,$('#searchinput').val());
+          window.open(url);
+      });
+      /* 鼠标聚焦时，回车事件 */
+      $("#searchinput").bind("keypress", function(){
+          if (event.keyCode == 13){
+          // 触发需要调用的方法
+          $(".search").click();
+          }
+      });
+      $('#menubtn').on('click', function (e) {
+          $('#seller').modal('show');
+      });
+    </script>
+  </body>
+
+  </html>`
+}
